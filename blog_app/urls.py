903fd59f0c0cfe63680cb1401ade5731e8c5ae3f
parthsoni <utsavlthakkar18@gnu.ac.in>
2022-07-30 23:15:04 -0600

    base template

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("base/", views.base, name="base"),
     path("about/", views.about, name="about"),
     path("blog/", views.blog, name="blog"),
     path("contact/", views.contact, name="contact"),
