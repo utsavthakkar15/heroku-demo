591b3e8c542fcd0f95581ffcb77bb2f5bd9b6530
parthsoni <utsavlthakkar18@gnu.ac.in>
2022-07-31 00:45:54 -0600

    login signup

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -7,6 +7,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("", views.index, name="index"),
     path("base/", views.base, name="base"),
+    path("login/", views.login, name="login"),
+    path("signup/", views.signup, name="signup"),
     path("about/", views.about, name="about"),
     path("blog/", views.blog, name="blog"),
     path("contact/", views.contact, name="contact"),
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,11 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponseRedirect, reverse
 from django.http import HttpResponse
-
+from django.contrib.auth import login, authenticate
+from .forms import CreateUserForm
 
 # Create your views here.
 
 def base(request):
     return render(request,'base.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        
+        if user:
+            if user.is_active:
+                # login(request,user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse("Your account is not active.")
+        else:
+            return HttpResponse("Invalid login details supplied.")
+
+    else:
+        return render(request,'login.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            # login(request, user)
+            return redirect('login')
+    else:
+        form = CreateUserForm()
+    return render(request, 'signup.html', {'form': form})
 
 
 def index(request):
