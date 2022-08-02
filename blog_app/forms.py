183464f48a0d1ef564157b5d8883e1fb299a78b5
parthsoni <utsavlthakkar18@gnu.ac.in>
2022-08-02 03:46:24 -0600

    template design

--- blog_app/forms.py
+++ blog_app/forms.py
@@ -4,22 +4,32 @@ from django.contrib.auth.forms import UserCreationForm
 
 from .models import Blog
 
+
 class CreateUserForm(UserCreationForm):
-    email = forms.EmailField(max_length=200, help_text='Required')
+    email = forms.EmailField(max_length=200, help_text="Required")
+
     class Meta:
-            model = User
-            fields = ['username', 'email', 'password1', 'password2']
+        model = User
+        fields = ["username", "email", "password1", "password2"]
 
 
 class LoginForm(forms.Form):
-    username=forms.CharField()
-    password=forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
     class Meta:
         model = User
-        fields = ('username', 'password' )
+        fields = ("username", "password")
 
 
 class AddBlog(forms.ModelForm):
     class Meta:
-        model=Blog
-        fields=('blog_tilte','blog_description','blog_image','blog_tagLine','blog_category','blog_tag','slug')
+        model = Blog
+        fields = (
+            "blog_tilte",
+            "blog_description",
+            "blog_image",
+            "blog_tagLine",
+            "blog_category",
+            "blog_tag",
+        )
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,5 +1,11 @@
 from multiprocessing import context
-from django.shortcuts import get_object_or_404, render, redirect, HttpResponseRedirect, reverse
+from django.shortcuts import (
+    get_object_or_404,
+    render,
+    redirect,
+    HttpResponseRedirect,
+    reverse,
+)
 from django.http import HttpResponse
 from django.contrib.auth import login, authenticate
 from blog_app.models import Blog, BlogCategory, BlogTags
@@ -7,90 +13,103 @@ from .forms import CreateUserForm, AddBlog
 
 # Create your views here.
 
+
 def base(request):
-    return render(request, 'base.html')
+    return render(request, "base.html")
 
 
 def login(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+    if request.method == "POST":
+        username = request.POST.get("username")
+        print(username)
+        password = request.POST.get("password")
+        print(password)
         user = authenticate(username=username, password=password)
-        
+        print(user)
         if user:
             if user.is_active:
                 # login(request,user)
-                return HttpResponseRedirect(reverse('index'))
+                return HttpResponseRedirect(reverse("index"))
             else:
                 return HttpResponse("Your account is not active.")
         else:
             return HttpResponse("Invalid login details supplied.")
 
     else:
-        return render(request,'login.html')
+        return render(request, "login.html")
 
 
 def signup(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password1")
+            email = form.cleaned_data.get("email")
             user = authenticate(username=username, password=raw_password)
             # login(request, user)
-            return redirect('login')
+            return redirect("login")
     else:
         form = CreateUserForm()
-    return render(request, 'signup.html', {'form': form})
+    return render(request, "signup.html", {"form": form})
 
 
 def index(request):
-    blog_category_display=BlogCategory.objects.all().order_by('-categoty_id')
-    blog_tag_display=BlogTags.objects.all().order_by('-tag_id')
-    blogs=Blog.objects.all().values('blog_category__categoty_type','blog_image','blog_tilte','created_at','slug').order_by('-blog_id')
-    context={
-        'blog_category_display':blog_category_display,
-        'blog_tag_display':blog_tag_display,
-        'blog':blogs,
+    blog_category_display = BlogCategory.objects.all().order_by("-categoty_id")
+    blog_tag_display = BlogTags.objects.all().order_by("-tag_id")
+    blogs = (
+        Blog.objects.all()
+        .values(
+            "blog_category__categoty_type",
+            "blog_image",
+            "blog_tilte",
+            "created_at",
+            "slug",
+        )
+        .order_by("-blog_id")
+    )
+    context = {
+        "blog_category_display": blog_category_display,
+        "blog_tag_display": blog_tag_display,
+        "blog": blogs,
     }
-    
-    return render(request,'index.html',context)
 
-def blog_category(request,slug):
-    blog_category=BlogCategory.objects.get(slug=slug)
-    context={
-        'blog_category':blog_category
-    }
-    return render(request,'base.html',context)
-def blog_tags(request,slug):
-    blog_tags=BlogTags.objects.get(slug=slug)
-    context={
-        'blog_tag':blog_tags
-    }
-    return render(request,'base.html',context)
+    return render(request, "index.html", context)
+
+
+def blog_category(request, slug):
+    blog_category = BlogCategory.objects.get(slug=slug)
+    context = {"blog_category": blog_category}
+    return render(request, "base.html", context)
+
+
+def blog_tags(request, slug):
+    blog_tags = BlogTags.objects.get(slug=slug)
+    context = {"blog_tag": blog_tags}
+    return render(request, "base.html", context)
+
+
 def about(request):
-    return render(request,'about.html')
+    return render(request, "about.html")
 
 
 def blog(request):
-    return render(request,'blog.html')
+    return render(request, "blog.html")
 
 
 def contact(request):
-    return render(request,'contact.html')
+    return render(request, "contact.html")
 
 
-def blogdetail(request,slug):
-    blog=get_object_or_404(Blog,slug=slug)
-    
-    context={
-        'blog':blog,
-        
-        
+def blogdetail(request, slug):
+    blog = get_object_or_404(Blog, slug=slug)
+
+    context = {
+        "blog": blog,
     }
-    
-    return render(request,'post_details.html',context)
+
+    return render(request, "post_details.html", context)
 
 
 def add_blog(request):
@@ -99,5 +118,5 @@ def add_blog(request):
     form = AddBlog(request.POST or None, request.FILES or None)
     if form.is_valid():
         form.save()
-    context['form'] = form
-    return render(request, 'addblog.html', context)
+    context["form"] = form
+    return render(request, "addblog.html", context)
